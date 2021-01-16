609aa80046c42005dc682439bee93246770ab9b7
Alexander <mangaka585@gmail.com>
2021-01-17 02:57:50 +0300

    Написал скрипт по обработке базы данных Кодик, сейчас база аниме насчитывает 9007. Но они пока не имеют серий. Промежуточный коммит так сказать..

--- services/Kodik-parse/parsebase.js
+++ services/Kodik-parse/parsebase.js
@@ -1,26 +1,21 @@
+global.fetch = require("node-fetch");
 var fs = require('fs');
-const RegExp1 = /\s/g;
-const RegExp2 = /\'/g;
-const RegExp3 = /\:/g;
-const RegExp4 = /\(/g;
-const RegExp5 = /\)/g;
-const RegExp6 = /-/g;
-const RegExp7 = /--/g;
-const RegExp8 = /\!/g;
-const RegExp9 = /\?/g;
+const RegExp1 = /[.,\/#!?∞$%\^&\*;:{}=\-_`~()]/g;
+const RegExp2 = /\s/g;
 
-const Parse = function(file){
-	let animeArray = [];
+const Parse = function(){
+	let Animelist = {
+		animelist : []
+	}
 	fs.readFile('./anime-serial.json', 'utf8', function (err, data) {
 	    if (err) console.log("Error while reading anime-serial file!", err);
-	    let animes = JSON.parse(data);
-	    let results = animes;
+		let results = JSON.parse(data);
 
-	    function Season(
+	    /*function Season(
 	    	number
 	    	) {
 	    	this.number = number;
-	    }
+	    }*/
 		
 		function Anime(
 			id, 
@@ -63,29 +58,29 @@ const Parse = function(file){
 		//Аниме
 	    for (key in results) {
 		  if (results.hasOwnProperty(key)) {
-		    let obj = results[key];
-		    if(obj.material_data == true){
+			let obj = results[key];
+		    if(obj.material_data !== undefined){
 			    let anime = new Anime(
 			    	obj.id.replace('serial-',''), 
-			    	obj.title_en == undefined ? "" : obj.title_en.replace(RegExp6,'').replace(RegExp1, '-').replace(RegExp2,'').replace(RegExp3,'').replace(RegExp4,'').replace(RegExp5,'').replace(RegExp7,'-').replace(RegExp8,'').replace(RegExp9,''),
+					obj.material_data.title_en === undefined ? "" : obj.material_data.title_en.replace(RegExp1,'').replace(RegExp2,'-').toLowerCase(),
 			    	obj.title === undefined ? "" : obj.title, 
 			    	obj.title_orig === undefined ? "" : obj.title_orig,
 			    	obj.year === undefined ? "" : obj.year, 
 			    	obj.last_season === undefined ? "" : obj.last_season,
 			    	obj.last_episode === undefined ? "" : obj.last_episode,
 			    	obj.episodes_count === undefined ? "" : obj.episodes_count,
-			    	obj.material_data === undefined || obj.material_data.poster_url === undefined ? "" : obj.material_data.poster_url,
-			    	obj.material_data === undefined || obj.material_data.duration === undefined ? "" : obj.material_data.duration,
-			    	obj.material_data === undefined || obj.material_data.anime_genres[0] === undefined ? "" : obj.material_data.anime_genres[0],
-			    	obj.material_data === undefined || obj.material_data.anime_genres[1] === undefined ? "" : obj.material_data.anime_genres[1],
-			    	obj.material_data === undefined || obj.material_data.anime_genres[2] === undefined ? "" : obj.material_data.anime_genres[2],
-			    	obj.material_data === undefined || obj.material_data.imdb_rating === undefined ? "" : obj.material_data.imdb_rating,
-			    	obj.material_data === undefined || obj.material_data.description === undefined ? "" : obj.material_data.description,
-			    	obj.material_data === undefined || obj.updated_at === undefined ? "" : obj.updated_at
+			    	obj.material_data.poster_url === undefined ? "" : obj.material_data.poster_url,
+			    	obj.material_data.duration === undefined ? "" : obj.material_data.duration,
+			    	obj.material_data.anime_genres === undefined || obj.material_data.anime_genres[0] === undefined ? "" : obj.material_data.anime_genres[0],
+			    	obj.material_data.anime_genres === undefined || obj.material_data.anime_genres[1] === undefined ? "" : obj.material_data.anime_genres[1],
+			    	obj.material_data.anime_genres === undefined || obj.material_data.anime_genres[2] === undefined ? "" : obj.material_data.anime_genres[2],
+			    	obj.material_data.imdb_rating === undefined ? "" : obj.material_data.imdb_rating,
+			    	obj.material_data.description === undefined ? "" : obj.material_data.description,
+			    	obj.updated_at === undefined ? "" : obj.updated_at
 			    	);
 
 				//Его сезоны + эпизоды
-				for (season in obj.seasons) {
+				/*for (season in obj.seasons) {
 				  if (obj.seasons.hasOwnProperty(season)) {
 				  	let seasonObj = new Season(season);
 
@@ -96,25 +91,25 @@ const Parse = function(file){
 
 				  	anime.seasons = seasonObj;
 				  }
-				}
+				}*/
 
-			    animeArray.push(anime);
+			    Animelist.animelist.push(anime);
 			}
 		  } 
 		}
 
 		//Переводим в JSON
-	    let exp = JSON.stringify(animeArray);
+		let exp = JSON.stringify(Animelist);
 
 		//Пишем результат в файл
 		fs.writeFile("./resultbaseDB.json", exp, function(err) {
 		    if(err) {
-		        return console.log("Error while resultDB.json file writing - " + err);
+		        return console.log("Error while resultbaseDB.json file writing - " + err);
 		    }
-		    console.log("The file resultDB.json was saved!");
+		    console.log("The file resultbaseDB.json was saved!");
 		});
 
 	})
 }
 
-Parse('./anime-serialme.json');
+Parse();
